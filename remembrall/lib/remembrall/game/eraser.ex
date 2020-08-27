f164e52815e3b3eed16cd9eeed3148c3bae5faf6
boggsey <eric@mlemedia.com>
2020-08-27 17:15:58 -0400

    Adding erase

--- remembrall/lib/remembrall/game/eraser.ex
+++ remembrall/lib/remembrall/game/eraser.ex
@@ -16,4 +16,20 @@ defmodule Remembrall.Game.Eraser do
     |> Kernel./(steps)
     |> ceil
   end
+
+  def erase(%{text: text, plan: [step | plan]}) do
+    new_text = erase_text(text, step)
+    %__MODULE__{text: new_text, plan: plan}
+  end
+
+  defp replace_character(_character, true), do: "_"
+  defp replace_character(character, false), do: character
+
+  defp erase_text(text, step) do
+    text
+    |> String.graphemes()
+    |> Enum.with_index(1)
+    |> Enum.map(fn {character, index} -> replace_character(character, index in step) end)
+    |> Enum.join()
+  end
 end
