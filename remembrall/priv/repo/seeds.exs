a76ce6639ad41d5a84f2bdf5de047f3f8d03ac3d
Bruce Tate <bruce@rapidred.com>
2020-08-27 14:29:17 -0400

    seeds

--- remembrall/priv/repo/seeds.exs
+++ remembrall/priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+Repo.insert! %Passage{
+    title: "IT Crowd",
+    text: "Have you tried turning it off and on again?",
+    steps: 3
+  }
+
+Repo.insert! %Passage{
+  title: "LOTR",
+  text: "One does not simply walk into Mordor",
+  steps: 4
+}
+
+Repo.insert! %Passage{
+  title: "StarWars",
+  text: "I am your father",
+  steps: 2
+}
+  
