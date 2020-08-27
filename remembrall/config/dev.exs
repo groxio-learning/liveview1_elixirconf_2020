6b3565ba32412a47257994e56f0e3fdd871dc1ba
Clive Harber <clive@whitemonkeysoftware.co.uk>
2020-08-27 22:36:44 +0100

    adding the eraser to liveview

--- remembrall/config/dev.exs
+++ remembrall/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :remembrall, Remembrall.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "isaacpaul",
   database: "remembrall_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
